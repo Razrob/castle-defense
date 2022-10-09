f88b856bc6a92b1a5bd806d9b6f860e0d24dca7a
AndreyLua <panzar.egorov1@yandex.ru>
2022-10-09 14:32:56 +0300

    Revert "player animation"
    This reverts commit 4068ec09cefab2a44cd656802ee72892bc19d5af.

--- Assets/Source/Scripts/Player/Movement/CommonStateMovement.cs
+++ Assets/Source/Scripts/Player/Movement/CommonStateMovement.cs
@@ -9,13 +9,12 @@ public class CommonStateMovement: IMovementState
         CharacterController characterController = playerData.Player.CharacterController;
 
         Vector3 direction = userInput.CalculateWorldDirection(movement.Camera).normalized;
-        Vector3 offset = movement.Speed * Time.deltaTime * direction * userInput.JoystickOffcet.magnitude + Physics.gravity * Time.deltaTime;
+        Vector3 offset = movement.Speed * Time.deltaTime * direction + Physics.gravity * Time.deltaTime;
       
         characterController.Move(offset);
-        
+     
         float joystickAngle = Mathf.Atan2(userInput.JoystickOffcet.x, userInput.JoystickOffcet.y) * 180 / Mathf.PI;
 
-        if (userInput.JoystickOffcet!=new Vector2(0,0))
-            playerData.Player.Transform.rotation = Quaternion.Euler(new Vector3(0, joystickAngle, 0));
+        playerData.Player.Transform.rotation = Quaternion.Euler(new Vector3(0, joystickAngle, 0));
     }
 }
--- Assets/Source/Scripts/Player/Movement/PlayerMovementController.cs
+++ Assets/Source/Scripts/Player/Movement/PlayerMovementController.cs
@@ -1,7 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovementController : CycleInitializerBase
 {
+    [SerializeField] private float _speed;
     [SerializeField] private Camera _camera;
 
     private Movement _movement;
@@ -9,7 +12,11 @@ public class PlayerMovementController : CycleInitializerBase
     protected override void OnInit() 
     {
         FWC.GlobalData.PlayerData.Player = FindObjectOfType<Player>();
-        _movement = new Movement(new CommonStateMovement(), _camera, FWC.GlobalData.PlayerData.Player.Speed);
+    }
+
+    private void Awake()
+    {
+        _movement = new Movement(new CommonStateMovement(),_camera,_speed);
     }
 
     private void Update()
