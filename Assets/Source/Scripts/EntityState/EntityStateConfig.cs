9539cccb01fc35f210c89f1b8efd40623703dc1d
AndreyLua <panzar.egorov1@yandex.ru>
2022-10-09 14:15:53 +0300

    entity state

---
+++ Assets/Source/Scripts/EntityState/EntityStateConfig.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "EntityStateConfig", menuName = "Config/EntityStateConfig")]
+public class EntityStateConfig : ScriptableObject
+{
+    public enum EntityState {Move,Build,Atack};
+    public EntityState[] States;
+}
+
---
+++ Assets/Source/Scripts/EntityState/EntityStatesHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static EntityStateConfig;
+
+public class EntityStatesHandler
+{
+    private EntityState[] _entityState;
+    private EntityState _startEntityState;
+
+    public event Action StateOnСhanged;
+
+    public EntityState StartEntityState {
+        get
+        {
+            return _startEntityState;
+        }
+        set
+        {
+            StateOnСhanged?.Invoke();
+            _startEntityState = value;
+        }
+   
+    }
+  
+
+    public EntityStatesHandler(EntityStateConfig entityState)
+    {
+        _entityState = entityState.States;
+    }
+}
